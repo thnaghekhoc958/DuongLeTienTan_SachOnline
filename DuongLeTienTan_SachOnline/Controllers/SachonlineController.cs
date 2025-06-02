887431ac35a3ddbeb5936f5f13765c30c67909da
Tan Nguyen <dongn0808@gmail.com>
2025-06-02 15:54:47 +0700

    Add project files.

---
+++ DuongLeTienTan_SachOnline/Controllers/SachonlineController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuongLeTienTan_SachOnline.Controllers
+{
+    public class SachonlineController : Controller
+    {
+        // GET: Sachonline
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+		public ActionResult ChudePartial()
+		{
+
+			return View();
+		}
+
+		public ActionResult NavPartial()
+		{
+			return PartialView();
+		}
+
+		public ActionResult SliderPartial()
+		{
+			return PartialView();
+		}
+		public ActionResult NhaXuatBanPartial()
+		{
+			return PartialView();
+		}
+		public ActionResult SachBanNhieuPartial()
+		{
+			return PartialView();
+		}
+		public ActionResult FooterPartial()
+		{
+			return PartialView();
+		}
+
+
+	}
+}
---
+++ DuongLeTienTan_SachOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DuongLeTienTan_SachOnline.MvcApplication" Language="C#" %>
